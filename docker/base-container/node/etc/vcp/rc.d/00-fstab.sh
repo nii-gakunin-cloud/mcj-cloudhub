06c6ee2e3f45db01a03e38becfd72ad1d71322a4
terayamaaaa <terayama@ascade.co.jp>
2023-09-06 20:43:54 +0900

    fix: Same modify for VC node image modification on coursewarehub
    Fixed a bug where the same description is written multiple times in /etc/fstab of a worker node.

--- docker/base-container/node/etc/vcp/rc.d/00-fstab.sh
+++ docker/base-container/node/etc/vcp/rc.d/00-fstab.sh
@@ -2,23 +2,8 @@
 
 : ${NFS_SERVER:?"NFS_SERVER must be set!"}
 
-grep -wq /mnt/nfs /etc/fstab
-if [ $? != 0 ]; then
-cat >> /etc/fstab <<EOF
+cat > /etc/fstab <<EOF
 ${NFS_SERVER}:/ /mnt/nfs nfs rw,_netdev,auto 0 0
-EOF
-fi
-
-grep -wq /jupytershare /etc/fstab
-if [ $? != 0 ]; then
-cat >> /etc/fstab <<EOF
 /mnt/nfs/jupytershare /jupytershare none bind 0 0
-EOF
-fi
-
-grep -wq /home /etc/fstab
-if [ $? != 0 ]; then
-cat >> /etc/fstab <<EOF
 /mnt/nfs/home /home none bind 0 0
 EOF
-fi
